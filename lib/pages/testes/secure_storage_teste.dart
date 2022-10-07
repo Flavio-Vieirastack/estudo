78009df8f14d4f73247581759599e9b5cda7eea8
Flavio Vieira <flavioemerson1@outlook.com>
2022-10-07 14:03:29 -0300

    adicionado mais casos de estudo

---
+++ lib/pages/testes/secure_storage_teste.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SecureStorageTeste extends StatefulWidget {
+
+  const SecureStorageTeste({ Key? key }) : super(key: key);
+
+  @override
+  State<SecureStorageTeste> createState() => _SecureStorageTesteState();
+}
+
+class _SecureStorageTesteState extends State<SecureStorageTeste> {
+   @override
+   Widget build(BuildContext context) {
+       return Scaffold(
+           appBar: AppBar(title: const Text(''),),
+           body: Container(),
+       );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,1 +1,28 @@
+import 'package:flutter_test/flutter_test.dart';
 
+class ClasseSoma {
+  int soma({required int primeiroNumero, required int segundoNumero}) {
+    return primeiroNumero + segundoNumero;
+  }
+
+  void retornaVoid() {}
+}
+
+void main() {
+  test('description1', () {
+    //arrange
+    final clase = ClasseSoma();
+    //act
+    final sut = clase.soma(primeiroNumero: 2, segundoNumero: 2);
+    //expect
+    expect(sut, 4);
+  });
+  test('description2', () {
+    //arrange
+    final clase = ClasseSoma();
+    //act
+    final sut = clase.retornaVoid;
+    //expect
+    expect(() => sut(), isA<void>());
+  });
+}
